585b6a7bd5b3c28563454bb9a986c4d882602f02
Josh Grainge <josh.grainge@hotmail.com>
2024-10-10 00:14:25 -0400

    Added distance conversion prototype

--- UnitConversionApp/UnitConversionApp.cpp
+++ UnitConversionApp/UnitConversionApp.cpp
@@ -16,6 +16,8 @@ protected:
 public:
 	double amount = 0;
 
+	DistanceUnit() {}
+
 	DistanceUnit(double _amount)
 	{
 		amount = _amount;
@@ -61,6 +63,58 @@ public:
 	}
 };
 
+class Metre : public DistanceUnit
+{
+public:
+	Metre(double amount) : DistanceUnit(amount)
+	{
+		kilometresMultiplier = 0.001;
+		metresMultiplier = 1;
+		centimetresMultiplier = 100;
+		milesMultiplier = 1.0/1609;
+		yardsMultiplier = 1.0/1.094;
+	}
+};
+
+class Centimetre : public DistanceUnit
+{
+public:
+	Centimetre(double amount) : DistanceUnit(amount)
+	{
+		kilometresMultiplier = 1.0/100000;
+		metresMultiplier = 0.01;
+		centimetresMultiplier = 1;
+		milesMultiplier = 1.0/160900;
+		yardsMultiplier = 1.0/91.44;
+	}
+};
+
+class Mile : public DistanceUnit
+{
+public:
+	Mile(double amount) : DistanceUnit(amount)
+	{
+		kilometresMultiplier = 1.60934;
+		metresMultiplier = 1609.34;
+		centimetresMultiplier = 160934;
+		milesMultiplier = 0.621371;
+		yardsMultiplier = 1760;
+	}
+};
+
+class Yard : public DistanceUnit
+{
+public:
+	Yard(double amount) : DistanceUnit(amount)
+	{
+		kilometresMultiplier = 1.0/1094;
+		metresMultiplier = 1.0/1.094;
+		centimetresMultiplier = 91.44;
+		milesMultiplier = 1.0/1760;
+		yardsMultiplier = 1093.613298;
+	}
+};
+
 using namespace std;
 
 enum UnitCategories { Distance, Energy, Mass, Time, Temperature, Area, Volume };
@@ -72,6 +126,9 @@ void DisplayMenu();
 void ChoiceMenu();
 
 void DisplayUnitsInCategory(UnitCategories category);
+
+void DistanceConversionLogic(int unitOne, int unitTwo, double amount);
+
 
 int main()
 {
@@ -113,47 +170,38 @@ int main()
 	double amount;
 	cin >> amount;
 
-
-	// Just testing kilometre conversion stuff
-	Kilometre km = Kilometre(amount);
-	cout << "Converted to metres: " << km.ConvertToMetres() << endl;
-	cout << "Converted to centimetres: " << km.ConvertToCentimetres() << endl;
-	cout << "Converted to miles: " << km.ConvertToMiles() << endl;
-	cout << "Converted to yards: " << km.ConvertToYards() << endl;
-
-
-	switch (unitCategoryInput)
+	switch (unitCategory)
 	{
-	case Distance:
+		case Distance:
+			DistanceConversionLogic(unitOneType, unitTwoType, amount);
+		break;
+
+		case Energy:
 
 		break;
 
-	case Energy:
+		case Mass:
 
 		break;
 
-	case Mass:
+		case Time:
 
 		break;
 
-	case Time:
+		case Temperature:
 
 		break;
 
-	case Temperature:
+		case Area:
 
 		break;
 
-	case Area:
+		case Volume:
 
 		break;
 
-	case Volume:
-
-		break;
-
-	defaut:
-		cout << "Invalid entry please try again\n";
+		defaut:
+			cout << "Invalid entry please try again\n";
 		break;
 	}
 
@@ -185,3 +233,74 @@ void DisplayUnitsInCategory(UnitCategories category)
 		cout << "[" << i << "] " << units[category][i] << endl;
 	}
 }
+
+# pragma region ConversionLogic
+void DistanceConversionLogic(int unitOne, int unitTwo, double amount)
+{
+	DistanceUnit unit;
+	string unitOneSuffix, unitTwoSuffix;
+	double convertedAmount = 0;
+
+	// TODO make this more elegant so that its not case 0. I don't want to make it strings either though due to spelling errors easily causing problems
+	// Create object for first unit
+	switch (unitOne)
+	{
+		// KM
+		case 0:
+			unit = Kilometre(amount);
+			break;
+		
+		// M
+		case 1:
+			unit = Metre(amount);
+			break;
+		
+		// CM
+		case 2:
+			unit = Centimetre(amount);
+			break;
+		
+		// Mi
+		case 3:
+			unit = Mile(amount);
+			break;
+		
+		// Yards
+		case 4:
+			unit = Yard(amount);
+			break;
+	}
+
+	// Convert unit
+	switch (unitTwo)
+	{
+		// to KM
+	case 0:
+		convertedAmount = unit.ConvertToKilometres();
+		break;
+
+		// to M
+	case 1:
+		convertedAmount = unit.ConvertToMetres();
+		break;
+
+		// to CM
+	case 2:
+		convertedAmount = unit.ConvertToCentimetres();
+		break;
+
+		// to Mi
+	case 3:
+		convertedAmount = unit.ConvertToMiles();
+		break;
+
+		// to Yards
+	case 4:
+		convertedAmount = unit.ConvertToYards();
+		break;
+	}
+
+	cout << "The amount " << amount << unitOneSuffix << " equals " << convertedAmount << unitTwoSuffix << endl;
+}
+
+#pragma endregion
